2949f6d9480ca4c15658ef923dc22763a7f544b5
Siddhesh Kulkarni <sidd32kul@gmail.com>
2020-11-15 19:09:22 +0530

    Add unit tests for is functions

--- test.js
+++ test.js
@@ -3,47 +3,155 @@ const Utilify = require('./index');
 
 describe('Node/JS Utilify', () => {
   describe('is Functions', () => {
-    it('is Array true', (done) => {
-      Utilify.isArray([]).should.equal(true);
-      done();
+    describe('Array', () => {
+      it('is Array true', (done) => {
+        Utilify.isArray([]).should.equal(true);
+        done();
+      });
+      it('is Array object false', (done) => {
+        Utilify.isArray({}).should.equal(false);
+        done();
+      });
+      it('is Array boolean false', (done) => {
+        Utilify.isArray(true).should.equal(false);
+        done();
+      });
+      it('is Array string false', (done) => {
+        Utilify.isArray('[]').should.equal(false);
+        done();
+      });
+      it('is Array number false', (done) => {
+        Utilify.isArray(10).should.equal(false);
+        done();
+      });
+      it('is Array function false', (done) => {
+        Utilify.isArray(() => { }).should.equal(false);
+        done();
+      });
     });
-    it('is Array false', (done) => {
-      Utilify.isArray({}).should.equal(false);
-      done();
+
+    describe('Number', () => {
+      it('is Number 5 true', (done) => {
+        Utilify.isNumber(5).should.equal(true);
+        done();
+      });
+      it('is Number NaN true', (done) => {
+        Utilify.isNumber('a5' * 5).should.equal(true);
+        done();
+      });
+      it('is Number object false', (done) => {
+        Utilify.isNumber({}).should.equal(false);
+        done();
+      });
+      it('is Number boolean false', (done) => {
+        Utilify.isNumber(true).should.equal(false);
+        done();
+      });
+      it('is Number string false', (done) => {
+        Utilify.isNumber('[]').should.equal(false);
+        done();
+      });
+      it('is Number array false', (done) => {
+        Utilify.isNumber([]).should.equal(false);
+        done();
+      });
+      it('is Number function false', (done) => {
+        Utilify.isNumber(() => { }).should.equal(false);
+        done();
+      });
+    });
+
+    describe('Function', () => {
+      it('is Function 5 true', (done) => {
+        Utilify.isFunction(() => { }).should.equal(true);
+        done();
+      });
+      it('is Function NaN false', (done) => {
+        Utilify.isFunction('a5' * 5).should.equal(false);
+        done();
+      });
+      it('is Function object false', (done) => {
+        Utilify.isFunction({}).should.equal(false);
+        done();
+      });
+      it('is Function boolean false', (done) => {
+        Utilify.isFunction(true).should.equal(false);
+        done();
+      });
+      it('is Function string false', (done) => {
+        Utilify.isFunction('[]').should.equal(false);
+        done();
+      });
+      it('is Function array false', (done) => {
+        Utilify.isFunction([]).should.equal(false);
+        done();
+      });
+      it('is Function number false', (done) => {
+        Utilify.isFunction(10).should.equal(false);
+        done();
+      });
+    });
+
+    describe('String', () => {
+      it('is Function 5 true', (done) => {
+        Utilify.isString('abc').should.equal(true);
+        done();
+      });
+      it('is Function NaN false', (done) => {
+        Utilify.isString('a5' * 5).should.equal(false);
+        done();
+      });
+      it('is Function object false', (done) => {
+        Utilify.isString({}).should.equal(false);
+        done();
+      });
+      it('is Function boolean false', (done) => {
+        Utilify.isString(true).should.equal(false);
+        done();
+      });
+      it('is Function function false', (done) => {
+        Utilify.isString(() => { }).should.equal(false);
+        done();
+      });
+      it('is Function array false', (done) => {
+        Utilify.isString([]).should.equal(false);
+        done();
+      });
+      it('is Function number false', (done) => {
+        Utilify.isString(10).should.equal(false);
+        done();
+      });
+    });
+
+    describe('Boolean', () => {
+      it('is Boolean 5 true', (done) => {
+        Utilify.isBoolean(true).should.equal(true);
+        done();
+      });
+      it('is Boolean NaN false', (done) => {
+        Utilify.isBoolean('a5' * 5).should.equal(false);
+        done();
+      });
+      it('is Boolean object false', (done) => {
+        Utilify.isBoolean({}).should.equal(false);
+        done();
+      });
+      it('is Boolean string false', (done) => {
+        Utilify.isBoolean('true').should.equal(false);
+        done();
+      });
+      it('is Boolean function false', (done) => {
+        Utilify.isBoolean(() => { }).should.equal(false);
+        done();
+      });
+      it('is Boolean array false', (done) => {
+        Utilify.isBoolean([]).should.equal(false);
+        done();
+      });
+      it('is Boolean number false', (done) => {
+        Utilify.isBoolean(10).should.equal(false);
+        done();
+      });
     });
   });
-
-  /* describe('Driver configuration', () => {
-    it('Connection string is configurable', (done) => {
-      MongoDriver.MongoDBManager.configure({
-        connectionString: connStr,
-        hasCert: hasCert,
-        certPath: certPath,
-        dbName: dbName
-      });
-      (MongoDriver.MongoDBManager.CONNECTION_STRING).should.equal(connStr);
-      done();
-    });
-
-    it('Database name is configurable', (done) => {
-      MongoDriver.MongoDBManager.configure({
-        connectionString: connStr,
-        hasCert: hasCert,
-        certPath: certPath,
-        dbName: dbName
-      });
-      (MongoDriver.MongoDBManager.DATABASE_NAME).should.equal(dbName);
-      done();
-    });
-
-    it('Certificate option is by default true', (done) => {
-      MongoDriver.MongoDBManager.configure();
-      (MongoDriver.MongoDBManager.HAS_CERT).should.equal(true);
-      done();
-    });
-  }); */
-
-  /* describe('Database operations', () => {
-
-  }); */
 });
